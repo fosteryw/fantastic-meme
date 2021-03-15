0a6b68047c343467e6b5cacb78d102d5acda1cfc
Foster Gingertail <koalavalera@mail.ru>
2021-03-15 17:12:37 +0300

    fixed

--- 1.py
+++ 1.py
@@ -1,16 +1,15 @@
-
 def formula_1(x1, y1, x2, y2):
     k = (y1 - y2) / (x1 - x2)
     b = y2 - k * x2
     return k, b
 
 
-z = int(input("Введите x1\n"))
-m = int(input("Введите x2\n"))
-n = int(input("Введите y1\n"))
-u = int(input("Введите y2\n"))
+z = int(input("Введите x1: "))
+m = int(input("Введите x2: "))
+n = int(input("Введите y1: "))
+u = int(input("Введите y2: "))
 
 k, b = formula_1(z, m, n, u)
-print("Координаты точки А(x1;y1)\n x1 = " ,z ,"\n y1 = " ,m, "\n")
-print("Координаты точки А(x2;y2)\n x2 = ", n, "\n y1 = ", u, "\n")
-print("Уравнение прямой, проходящей через эти точки\n y=", k, "*x+", b)
+print("Координаты точки А(x1;y1)\n x1 = {}\n y1 = {}\n".format(z, m))
+print("Координаты точки А(x2;y2)\n x2 = {}\n y1 = {}\n".format(n, u))
+print("Уравнение прямой, проходящей через эти точки\n y={}*x+{}".format(k, b))
