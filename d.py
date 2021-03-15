3ba7a2be0fd07af8d2a346e66540b4266c2f4f19
Foster Gingertail <koalavalera@mail.ru>
2021-03-15 16:50:17 +0300

    fixed

--- d.py
+++ d.py
@@ -1,36 +1,18 @@
-def formula_2(x3):
-    w = x3 ** 2
-    s = x3 ** 0.5
-    return w, s
 
-
-xy = 999
-w, s = formula_2(xy)
-print(w, s)
-
-
-def stroka(stra):
-    if not isinstance(stra, str):
-        return 'Error'
-    u = stra[3::5]
-    return u
-
-
-print(
-    stroka('asdasf'))
-
-print("sdffd".replace('ff', '89898'))
-print("as{}d".format(w))
 
 def formula_1(x1, y1, x2, y2):
     k = (y1 - y2) / (x1 - x2)
     b = y2 - k * x2
     return k, b
+z = int(input("Введите x1\n"))
+m = int(input("Введите x2\n"))
+n = int(input("Введите y1\n"))
+u = int(input("Введите y2\n"))
 
-
-o = 8
-k, b = formula_1(1, 3, 5, o)
-print(k, b)
+k, b = formula_1(z, m, n, u)
+print("Координаты точки А(x1;y1)\n x1 = ",z,"\n y1 = " ,m, "\n")
+print("Координаты точки А(x2;y2)\n x2 = ",n,"\n y1 = " ,u, "\n")
+print("Уравнение прямой, проходящей через эти точки\n y=",k,"*x+",b)
 
 
 
