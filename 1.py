93e1f6c4508bc7b2ce1103ceadb104347a0fe977
fosteryw <80309936+fosteryw@users.noreply.github.com>
2021-03-15 19:33:55 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,13 +3,13 @@ def formula_1(x1, y1, x2, y2):
     b = y2 - k * x2
     return k, b
 
+if __name__ == "__main__":
+    z = int(input("Введите x1: "))
+    m = int(input("Введите x2: "))
+    n = int(input("Введите y1: "))
+    u = int(input("Введите y2: "))
 
-z = int(input("Введите x1: "))
-m = int(input("Введите x2: "))
-n = int(input("Введите y1: "))
-u = int(input("Введите y2: "))
-
-k, b = formula_1(z, m, n, u)
-print("Координаты точки А(x1;y1)\n x1 = {}\n y1 = {}\n".format(z, m))
-print("Координаты точки А(x2;y2)\n x2 = {}\n y1 = {}\n".format(n, u))
-print("Уравнение прямой, проходящей через эти точки\n y={}*x+{}".format(k, b))
+    k, b = formula_1(z, m, n, u)
+    print("Координаты точки А(x1;y1)\n x1 = {}\n y1 = {}\n".format(z, m))
+    print("Координаты точки А(x2;y2)\n x2 = {}\n y1 = {}\n".format(n, u))
+    print("Уравнение прямой, проходящей через эти точки\n y={}*x+{}".format(k, b))
